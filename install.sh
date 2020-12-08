be4c60f209b27eed194ac58b4acfa8a82ef6e6e3
ama31337 <70890873+ama31337@users.noreply.github.com>
2020-12-08 20:26:45 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 SCRIPT_DIR=`cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P`
 . "${SCRIPT_DIR}/env.sh"
 
-sudo systemctl stop $SERVICE_NAME
+sudo systemctl stop ${SERVICE_NAME}
 mkdir -p ${BIN_PATH}
 cd ${BIN_PATH}
 
@@ -16,24 +16,24 @@ wget ${ENV_FILE} -O env.sh chmod u+x env.sh
 # setup service file
 wget ${UNIT_FILE_URL} -O ${SERVICE_NAME}
 sed -i "s/<user>/${USER}/g" spacemeshd.service
-sed -i "s#<work_dir>#"$BIN_PATH"#g" spacemeshd.service
-sudo cp ${SERVICE_NAME} /etc/systemd/system/$SERVICE_NAME
+sed -i "s#<work_dir>#"${BIN_PATH}"#g" spacemeshd.service
+sudo cp ${SERVICE_NAME} /etc/systemd/system/${SERVICE_NAME}
 sudo systemctl daemon-reload
 
 # get start sctipt
-wget $START_FILE -O start_daemon.sh && chmod u+x start_daemon.sh
+wget ${START_FILE} -O start_daemon.sh && chmod u+x start_daemon.sh
 
 # clear prev data
 rm -rf sm_data post_data
 
 # start service
-sudo systemctl enable $SERVICE_NAME
-sudo systemctl start $SERVICE_NAME
+sudo systemctl enable ${SERVICE_NAME}
+sudo systemctl start ${SERVICE_NAME}
 
 #create alias
-echo "alias spacestart='sudo systemctl start $SERVICE_NAME'" >> ~/.bash_aliases
-echo "alias spacestop='sudo systemctl stop $SERVICE_NAME'" >> ~/.bash_aliases
-echo "alias spacestatus='sudo systemctl status $SERVICE_NAME'" >> ~/.bash_aliases
+echo "alias spacestart='sudo systemctl start ${SERVICE_NAME}'" >> ~/.bash_aliases
+echo "alias spacestop='sudo systemctl stop ${SERVICE_NAME}'" >> ~/.bash_aliases
+echo "alias spacestatus='sudo systemctl status ${SERVICE_NAME}'" >> ~/.bash_aliases
 source ~/.bash_aliases
 
 
